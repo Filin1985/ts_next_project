d06d83f853bb0a60e80378b325480e0ec31d98b9
Marat Ikhsanov <filin1985@Marats-MacBook-Pro.local>
2023-08-26 13:05:11 +0300

    fix: Fix mistakes after second review

--- app/(site)/components/Header/Header.tsx
+++ app/(site)/components/Header/Header.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Image from 'next/image';
 import { HeaderProps } from './Header.props';
-import GitHubIcon from '@/public/icons/github.svg';
+import gitHubIcon from '@/public/icons/github.svg?url';
 import styles from './Header.module.css';
 
 export const Header = ({ ...props }: HeaderProps): JSX.Element => {
   return (
     <div className={styles.container} {...props}>
       <h1 className={styles.title}>.my_blog</h1>
-      <GitHubIcon />
+      <Image src={gitHubIcon} alt='Github'/>
     </div>
   );
 };
